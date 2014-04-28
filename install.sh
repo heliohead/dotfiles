1ba02f78cd8247c4c49a83c19f6216a156430e89
Helio Oliveira <hheelliioo@live.com>
2014-04-28 20:09:55 -0300

    Download and extrating files

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#
+# Install dotfiles @heliohead
+#
+
+set -e
 
 # dependencies
 dep=(curl unzip git)
@@ -7,18 +12,38 @@ dep=(curl unzip git)
 function message_exit(){
   red='\e[0;31m'
   NC='\e[0m' 
-  echo -e "${red} $1 ${NC}"
+  echo -e "${red}##=> $1 ${NC}"
   exit 1
+}
+
+# Make messages to current_status
+function current_status(){
+  blue='\e[0;34m'
+  NC='\e[0m' 
+  echo -e "${blue}##=> $1 ${NC}"
 }
 
 # Verify is dep is installed
 for i in "${dep[@]}"; do
 	if ! command -v $i >/dev/null; then
-	  message_exit "##=> You need to have $i installed"
+	  message_exit "You need to have $i installed"
 	fi 
 done
 
+# Some variables
+tmp_dir=/tmp/dotfiles-master
+compress_files=/tmp/dotfiles.zip
+src_files=https://github.com/heliohead/dotfiles/archive/master.zip
+bk_files="/tmp/yours_${RAMDOM}.sh"
 
+# Download files
+current_status "Download $compress_file to $tmp_dir"
+sleep 1
+curl -Ls -o $compress_files $src_files
 
+# Extracting 
+current_status "Extracting $compress_file"
+sleep 1
+unzip	-q $compress_files -d /tmp
 
 
