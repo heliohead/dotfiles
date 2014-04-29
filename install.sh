1175cbe77f0ee7c74b5a4b7655ca38d27e41f635
Helio Oliveira <hheelliioo@live.com>
2014-04-29 09:07:31 -0300

    instalation

--- install.sh
+++ install.sh
@@ -16,8 +16,8 @@ function message_exit(){
   exit 1
 }
 
-# Make messages to current_status
-function current_status(){
+# Make messages to info
+function info(){
   blue='\e[0;34m'
   NC='\e[0m' 
   echo -e "${blue}##=> $1 ${NC}"
@@ -32,18 +32,34 @@ done
 
 # Some variables
 tmp_dir=/tmp/dotfiles-master
-compress_files=/tmp/dotfiles.zip
+compress_file=/tmp/dotfiles.zip
 src_files=https://github.com/heliohead/dotfiles/archive/master.zip
 bk_files="/tmp/yours_${RAMDOM}.sh"
 
 # Download files
-current_status "Download $compress_file to $tmp_dir"
-sleep 1
-curl -Ls -o $compress_files $src_files
+info "Download $compress_file to $tmp_dir"
+curl -Ls -o $compress_file $src_files
 
 # Extracting 
-current_status "Extracting $compress_file"
-sleep 1
-unzip	-q $compress_files -d /tmp
+info "Extracting $compress_file"
+unzip	-q $compress_file -d /tmp
 
 
+# Make a backup file if exist folder
+if [[ ! -d ~/.dotfiles ]]; then
+	info "Coping new .dotfiles"
+	cp -R $tmp_dir ~/.dotfiles	
+else
+	info "Coping your .dotfiles to .dotfiles_old"
+	mv $HOME/.dotfiles $HOME/.dotfiles_old
+	info "Coping new .dotfiles"
+	cp -R $tmp_dir ~/.dotfiles
+fi
+
+# Sourcing new .dotfiles
+echo "source ~/.dotfiles/init.sh" >> ~/.bashrc
+source ~/.bashrc
+
+# Garbage colector :D
+info "Remove used files"
+rm -rf $tmp_dir && rm $compress_file
